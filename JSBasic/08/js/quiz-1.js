4618fb24afac501c1b13ef1fd3dfe31165180973
Lee Soyeon <data.sy.2@gmail.com>
2023-07-14 17:11:47 +0900

    8장 전체 복습 후 최종 quiz

---
+++ JSBasic/08/js/quiz-1.js
@@ -0,0 +1,7 @@
+var myText = document.querySelector("#myText");
+
+myText.onclick =  function(){
+    this.style.fontSize = "20px";
+    this.style.color = "blue";
+    this.style.backgroundColor = "#ccc";
+}
---
+++ JSBasic/09/js/quiz-1.js
@@ -0,0 +1,24 @@
+var price = 24000;
+var checkbx = document.querySelectorAll(".checkbx");
+var total = document.querySelector("#total");
+total.value = price+"원";
+
+for(let i=0; i<checkbx.length; i++){
+    checkbx[i].addEventListener("click", priceUpdate);
+//    console.log("가격 변화 : " + price)    
+}
+
+function priceUpdate(){
+    if(this.checked){
+        price += parseInt(this.value);
+        // console.log("체크 시 가격: " + price)
+    }else{
+        price -= parseInt(this.value);
+        // console.log("해제 시 가격: " + price)
+    }
+    // total의 위치는 수정한 즉시!!!
+    // 가격 변동 후 바로 담아주지 않으면 (= 이 함수를 빠져나가면) price 값 날라가나봐....=> why????
+    total.value = price+"원";
+    console.log("함수 안에서 가격 변화 : " + price)    
+}
+
