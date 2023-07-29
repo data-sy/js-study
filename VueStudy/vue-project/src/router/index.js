1bc67f85d9997aa6f002d829840060c3db474014
Lee Soyeon <data.sy.2@gmail.com>
2023-07-29 20:46:11 +0900

    4장 라우터

---
+++ VueStudy/vue-project/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DataBinding from '../views/DataBinding.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting : 라우트 레벨에서 코드를 분할하는 방법
+    // this generates a separate chunk (about.[hash].js) for this route
+      // 이 라우트에 대한 chunck 파일이 분리되어 생성됩니다.
+    // which is lazy-loaded when the route is visited.
+      // 이 라우트에 방문했을 때 lazy-load(지연 로드) 됩니다.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
