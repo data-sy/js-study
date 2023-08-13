29152d79a5bfc4d0e8aefff125d765d03dc37dcd
Lee Soyeon <data.sy.2@gmail.com>
2023-08-13 23:15:46 +0900

    화면 테스트 성공

---
+++ VueStudy/vue-mmt-test/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+
+import '@/assets/styles.scss';
+
+const app = createApp(App)
+app.use(PrimeVue, {ripple: true});
+
+app.use(router)
+
+app.mount('#app')
