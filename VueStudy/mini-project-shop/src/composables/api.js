f3cc7b02b38ac95426610dc574ab821d06850fd1
Lee Soyeon <data.sy.2@gmail.com>
2023-09-27 13:18:03 +0900

    포트 3000으로 수정

--- VueStudy/mini-project-shop/src/composables/api.js
+++ VueStudy/mini-project-shop/src/composables/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// axios.defaults.baseURL = 'http://15.164.232.32:8080/api/v1';
+// axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export function useApi() {
   // 기본 API URL을 설정합니다.
   const baseURL = 'http://15.164.232.32:8080/api/v1';
@@ -8,17 +12,14 @@ export function useApi() {
   const api = axios.create({
     baseURL,
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json;charset=utf-8',
     },
   });
 
   // GET 요청을 보내는 함수
-  // 데이터는 쿼리 문자열로
-  async function get(endpoint, data = null) {
+  async function get(endpoint) {
     try {
-      const response = await api.get(endpoint, {
-        params: data,
-      });
+      const response = await api.get(endpoint);
       return response.data;
     } catch (error) {
       throw new Error(`GET ${endpoint} failed: ${error.message}`);
